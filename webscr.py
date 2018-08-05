86d9946ce458cc577aed70894f5f0f66be39a529
narita-special0904@gmail.com <narita.special0904@gmail.com>
2018-08-06 06:53:42 +0900

    commit webscr.py

---
+++ webscr.py
@@ -0,0 +1,22 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scraper:
+	def __init__(self, site):
+		self.site = site
+
+	def scrape(self):
+		r = urllib.request.urlopen(selft.site)
+		html = r.read()
+		parser = "html.parser"
+		sp = BeautifulSoup( html, parser )
+
+		for tag in sp.find_all("a"):
+			utl = tag.get("href")
+			if url is None:
+				continue
+			if "html" in url:
+				print("\n" + url)
+
+news = "https://news.google.com"
+Scraper(news).scrape()
